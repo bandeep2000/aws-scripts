67738b12ccc76ba8a9bd5f966e3bf53e8fc67de9
bandeep dhillon <bandeepdhillon@bandeeps-iMac.local>
2017-12-24 07:26:54 -0800

    added regions

--- s3-buckets.py
+++ s3-buckets.py
@@ -1,7 +1,10 @@
 import boto3,sys
 import botocore
+import logging 
 from Variables import regions
- 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+#logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
 """
 Please make sure you have config file present to initialize your credentials
 for aws and region
@@ -17,20 +20,27 @@ region = us-east-1
 
 """
 
-
 def getBucketList(region = 'us-east-11') :         
+        
+        logging.info("Getting bucket list for region '%s'"%region)
+        print " "
+        #return 0
+
 	s3 = boto3.resource('s3',region_name= region)
 	
 	try:
 	   for bucket in s3.buckets.all():
 	      print(bucket.name)
 	except botocore.exceptions.EndpointConnectionError:
-           print "Not able to connect to region '%s'"%region
+           error_msg = "Not able to connect to region '%s'"%region
+           error_msg = error_msg + ", please make sure it is passed correctly"
+           print error_msg
            raise
     
 
 #getBucketList()
 
 for region in regions:
-  print region
+  getBucketList(region)
+  #print region
 
